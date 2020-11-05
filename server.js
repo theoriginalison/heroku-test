ddd2476c9695d775ace7551b6e719232a676c7dd
Alison Lubar <aslubar@gmail.com>
2020-11-05 18:19:01 -0500

    added files from Star Wars Final

---
+++ server.js
@@ -0,0 +1,101 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+//what is actually shown to the user from the UI perspective 
+//res is the second parameter in the callback function, and what's sent back (on line 49) is the callback function
+//__dirname joins it to the current directory-- JUST USE THIS CODE PERIODT: res.sendFile(path.join(__dirname, "view.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function (req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all characters
+app.get("/api/characters", function (req, res) {
+  return res.json(characters);
+});
+
+// Displays a single character, or returns false
+//the colon shows what's being referenced-- need to reference the variable on thel ine below it (63)
+app.get("/api/characters/:character", function (req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  //iterates through all of the characters, and if we don't find the character, it returns false
+  for (var i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newCharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newCharacter);
+
+  //we're adding to the characters array
+  //req.body will have the object you've sent in via the post route
+  characters.push(newCharacter);
+
+  res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
